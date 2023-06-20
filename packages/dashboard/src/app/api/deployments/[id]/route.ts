0149862319860a145654769d66825f876649f802
huming <humings@gmail.com>
2023-06-20 17:16:48 +0800

    basic api for control plane

--- packages/dashboard/src/app/api/deployments/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "~/server/db";
-
-export async function POST(req: NextRequest) {
-  const data = (await req.json()) as { environmentId: string };
-  return NextResponse.json({ data: data });
-}
