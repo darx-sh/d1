73fd175df36c051ba66165c7a8d0810704fcefef
huming <humings@gmail.com>
2023-07-29 00:10:41 +0800

    f

--- crates/core/tests/server/cljb3ovlt0002e38vwo0xi5ge/924/functions/hello2.js
+++
@@ -1,1 +0,0 @@
-export default function hello2() {return "hi2";}
--- crates/core/tests/server/cljb3ovlt0002e38vwo0xi5ge/926/__registry.js
+++
@@ -1,5 +0,0 @@
-
-import { default as functions_hello_default } from "./functions/hello.js";
-globalThis.functions_hello_default = functions_hello_default;
-import { default as functions_hello2_default } from "./functions/hello2.js";
-globalThis.functions_hello2_default = functions_hello2_default;
