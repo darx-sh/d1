2ef6cd71549a04e0c080575cb5240d579575f92b
Hu Ming <humings@gmail.com>
2023-08-15 09:17:25 +0800

    Ming (#100)
    * move tenant's ddl and dml to db crates

--- crates/core/tenants/mod.rs
+++ crates/core/tenants/mod.rs
@@ -1,12 +1,6 @@
 mod cache;
 mod deploy;
-mod sql;
 
 pub use deploy::{
   add_deployment, init_deployments, invoke_function, match_route,
-};
-
-pub use sql::{
-  ddl::{add_column, create_table, drop_column, drop_table, rename_column},
-  DxColumnType, DxDatum,
 };
