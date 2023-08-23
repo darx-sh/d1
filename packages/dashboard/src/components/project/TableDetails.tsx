e0cb2220c3b41115963ce935d6a756258e5b27c0
huming <humings@gmail.com>
2023-08-23 22:59:34 +0800

    show basic data

--- packages/dashboard/src/components/project/TableDetails.tsx
+++ packages/dashboard/src/components/project/TableDetails.tsx
@@ -1,3 +1,5 @@
+import { Row, useDatabaseState } from "./DatabaseContext";
+
 const people = [
   {
     name: "Lindsay Walton",
@@ -8,12 +10,61 @@ const people = [
   // More people...
 ];
 export default function TableDetails() {
+  const dbState = useDatabaseState();
+  const curTable = dbState.curData!.tableName;
+
+  const renderColumnNames = () => {
+    return (
+      <tr>
+        {dbState.schema[curTable]!.map((col, idx) => {
+          return (
+            <th
+              key={col}
+              scope="col"
+              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            >
+              {col}
+            </th>
+          );
+        })}
+        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+          <span className="sr-only">Edit</span>
+        </th>
+      </tr>
+    );
+  };
+
+  const renderOneRow = (row: Row, columnNames: string[], ridx: number) => {
+    return (
+      <tr key={ridx}>
+        {columnNames.map((name, idx) => {
+          return (
+            <td
+              key={idx}
+              className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+            >
+              {row[name]}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  };
+
+  const renderRows = () => {
+    const curTable = dbState.curData!.tableName;
+    const columnNames = dbState.schema[curTable]!;
+    return dbState.curData?.rows.map((row, idx) => {
+      return renderOneRow(row, columnNames, idx);
+    });
+  };
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Users
+            {curTable}
           </h1>
         </div>
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
@@ -37,63 +88,8 @@ export default function TableDetails() {
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <table className="min-w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Title
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Role
-                  </th>
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200">
-                {people.map((person) => (
-                  <tr key={person.email}>
-                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                      {person.name}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {person.title}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {person.email}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {person.role}
-                    </td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit<span className="sr-only">, {person.name}</span>
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
+              <thead>{renderColumnNames()}</thead>
+              <tbody className="divide-y divide-gray-200">{renderRows()}</tbody>
             </table>
           </div>
         </div>
