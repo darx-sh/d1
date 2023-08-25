014ddb71b57408722252d5fdd5f47cd222ec8c26
Hu Ming <humings@gmail.com>
2023-08-25 23:51:21 +0800

    Ming (#124)
    * move columnNames out
    
    * added a few buttons

--- packages/dashboard/src/components/project/TableDetails.tsx
+++ packages/dashboard/src/components/project/TableDetails.tsx
@@ -1,27 +1,19 @@
 import { Row, useDatabaseState } from "./DatabaseContext";
-
-const people = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  // More people...
-];
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 export default function TableDetails() {
   const dbState = useDatabaseState();
   const curTable = dbState.curData!.tableName;
+  const columnNames = dbState.schema[curTable]!;
 
-  const renderColumnNames = () => {
+  const renderColumnNames = (columnNames: string[]) => {
     return (
       <tr>
-        {dbState.schema[curTable]!.map((col, idx) => {
+        {columnNames.map((col, idx) => {
           return (
             <th
               key={col}
               scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+              className="border bg-gray-300 px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
             >
               {col}
             </th>
@@ -41,7 +33,7 @@ export default function TableDetails() {
           return (
             <td
               key={idx}
-              className="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
+              className="whitespace-nowrap border px-4 py-4 text-sm text-gray-500"
             >
               {row[name]}
             </td>
@@ -51,9 +43,7 @@ export default function TableDetails() {
     );
   };
 
-  const renderRows = () => {
-    const curTable = dbState.curData!.tableName;
-    const columnNames = dbState.schema[curTable]!;
+  const renderRows = (columnNames: string[]) => {
     return dbState.curData?.rows.map((row, idx) => {
       return renderOneRow(row, columnNames, idx);
     });
@@ -63,17 +53,12 @@ export default function TableDetails() {
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            {curTable}
-          </h1>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded bg-gray-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Edit Table
-          </button>
+          <div className="flex items-center">
+            <h1 className="p-2 text-base font-semibold leading-6 text-gray-900">
+              {curTable}
+            </h1>
+            <Cog6ToothIcon className="h-6 w-6 hover:bg-gray-600"></Cog6ToothIcon>
+          </div>
         </div>
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
           <button
@@ -85,10 +70,10 @@ export default function TableDetails() {
         </div>
       </div>
 
-      <div className="min-w-full overflow-auto py-2 align-middle sm:px-6 lg:px-8">
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead>{renderColumnNames()}</thead>
-          <tbody className="divide-y divide-gray-200">{renderRows()}</tbody>
+      <div className="overflow-auto py-2 align-middle">
+        <table>
+          <thead>{renderColumnNames(columnNames)}</thead>
+          <tbody>{renderRows(columnNames)}</tbody>
         </table>
       </div>
     </div>
--- packages/dashboard/src/components/project/TableList.tsx
+++ packages/dashboard/src/components/project/TableList.tsx
@@ -1,13 +1,6 @@
+import { useState } from "react";
 import { useDatabaseState, useDatabaseDispatch } from "./DatabaseContext";
-
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Documents", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
+import CreateTableModal from "~/components/project/CreateTableModal";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -16,36 +9,49 @@ function classNames(...classes: string[]) {
 export default function TableList() {
   const state = useDatabaseState();
   const dispatch = useDatabaseDispatch();
+  const [isCreateTable, setIsCreateTable] = useState(false);
 
   const navigation = Object.keys(state.schema).map((tableName: string) => {
     return { name: tableName, href: "#", current: false };
   });
 
   return (
-    <nav className="flex flex-col p-2" aria-label="Tables">
-      <button
-        type="button"
-        className="block rounded bg-gray-400 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Create Table
-      </button>
-      <ul role="list" className="-mx-2 space-y-1">
-        {navigation.map((item) => (
-          <li key={item.name}>
-            <a
-              href={item.href}
-              className={classNames(
-                item.current
-                  ? "bg-gray-50 text-indigo-600"
-                  : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
-                "group flex gap-x-3 rounded-md p-2 pl-3 text-sm font-semibold leading-6"
-              )}
-            >
-              {item.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      {isCreateTable && (
+        <CreateTableModal
+          onClose={() => {
+            setIsCreateTable(false);
+          }}
+        ></CreateTableModal>
+      )}
+      <nav className="flex flex-col p-2" aria-label="Tables">
+        <button
+          type="button"
+          className="mb-2 block rounded bg-gray-400 px-2 py-2 text-left text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={() => {
+            setIsCreateTable(true);
+          }}
+        >
+          Create Table
+        </button>
+        <ul role="list" className="space-y-1">
+          {navigation.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className={classNames(
+                  item.current
+                    ? "bg-gray-50 text-indigo-600"
+                    : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                  "group flex gap-x-3 rounded-md p-2 pl-3 text-sm font-semibold leading-6"
+                )}
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
