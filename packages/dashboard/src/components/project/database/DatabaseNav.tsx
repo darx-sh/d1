33f11fa651f8dd1c5077ec507154e8b1e9ae4a74
huming <humings@gmail.com>
2023-09-09 13:43:57 +0800

    better UI for schema

--- packages/dashboard/src/components/project/database/DatabaseNav.tsx
+++ packages/dashboard/src/components/project/database/DatabaseNav.tsx
@@ -1,5 +1,6 @@
 import { useDatabaseState, useDatabaseDispatch, Row } from "./DatabaseContext";
 import classNames from "classnames";
+import { ShareIcon, TableCellsIcon } from "@heroicons/react/20/solid";
 
 export default function DatabaseNav() {
   const state = useDatabaseState();
@@ -16,22 +17,30 @@ export default function DatabaseNav() {
 
   return (
     <>
-      <nav className="mb-6 mt-4 flex flex-col rounded p-2">
-        <ul className="space-y-1">
-          <li
-            className=""
-            onClick={() => {
-              dispatch({ type: "SetNav", nav: { typ: "Schema" } });
-            }}
+      <div className="mx-2 mt-6 rounded bg-gray-200 px-2 py-3">
+        <div
+          className={classNames(
+            state.curNav.typ === "Schema"
+              ? "text-indigo-600"
+              : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+            "flex items-center justify-center py-3"
+          )}
+          onClick={() => {
+            dispatch({ type: "SetNav", nav: { typ: "Schema" } });
+          }}
+        >
+          <ShareIcon className="h-5 w-5" />
+          <div className="px-2 text-sm font-semibold leading-6">Schema</div>
+        </div>
+      </div>
+
+      <div className="mx-2 mt-6 rounded bg-gray-200 py-4">
+        <div className="px-2 text-xs">Tables</div>
+        <nav className="flex flex-col px-2" aria-label="Tables">
+          <ul
+            role="list"
+            className="space-y-1 divide-y-2 border-b-2 border-t-2"
           >
-            Schema
-          </li>
-        </ul>
-      </nav>
-      <div className="">
-        <p className="p-2">Tables</p>
-        <nav className="flex flex-col p-2" aria-label="Tables">
-          <ul role="list" className="space-y-1">
             {navigation.map((item) => (
               <li
                 key={item.name}
@@ -51,7 +60,12 @@ export default function DatabaseNav() {
                     "group flex gap-x-3 rounded-sm p-2 pl-3 text-sm font-semibold leading-6"
                   )}
                 >
-                  {item.name}
+                  <div className="flex items-center">
+                    <div>
+                      <TableCellsIcon className="h-5 w-5"></TableCellsIcon>
+                    </div>
+                    <div className="px-2 text-sm">{item.name}</div>
+                  </div>
                 </a>
               </li>
             ))}
