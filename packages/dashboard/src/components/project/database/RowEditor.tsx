57f8f6a19ae697f9fbf23f5e05f5f4b9e4e69fa5
huming <humings@gmail.com>
2023-09-10 23:03:33 +0800

    f

--- packages/dashboard/src/components/project/database/RowEditor.tsx
+++ packages/dashboard/src/components/project/database/RowEditor.tsx
@@ -83,7 +83,9 @@ export default function RowEditor(props: RowEditorProps) {
                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                       <div className="px-4 sm:px-6">
                         <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                          Panel title
+                          {state.draftRowMod === "Create" && "New record"}
+                          {state.draftRowMod === "Update" &&
+                            "Update an exising row"}
                         </Dialog.Title>
                       </div>
                       <div className="relative mt-6 flex-1 px-4 sm:px-6">
