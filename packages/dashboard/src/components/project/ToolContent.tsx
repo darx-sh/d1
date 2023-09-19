faaa33c91f07514e50837835aee2dc919035798b
huming <humings@gmail.com>
2023-09-19 16:43:51 +0800

    use monaco-editor

--- packages/dashboard/src/components/project/ToolContent.tsx
+++ packages/dashboard/src/components/project/ToolContent.tsx
@@ -1,23 +1,8 @@
 import React from "react";
-import CodeMirror from "@uiw/react-codemirror";
-import { githubLight } from "@uiw/codemirror-theme-github";
-import { javascript } from "@codemirror/lang-javascript";
-import { EditorView } from "@codemirror/view";
 import { useProjectState, useProjectDispatch } from "./ProjectContext";
 import HttpEndpoints from "~/components/project/HttpEndpoints";
 import Database from "~/components/project/database/Database";
-
-const myTheme = EditorView.theme({
-  "&": {
-    fontSize: "1rem",
-    lineHeight: "1.5rem",
-    maxHeight: "670px",
-  },
-  "&.cm-focused": {
-    outline: "none",
-  },
-  ".cm-scroller": { overflow: "auto" },
-});
+import Editor from "@monaco-editor/react";
 
 export default function ToolContent() {
   const projectState = useProjectState()!;
@@ -27,23 +12,28 @@ export default function ToolContent() {
     const tab = projectState.tabs[tabIdx]!;
     switch (tab.type) {
       case "JsEditor": {
-        const code = projectState.directory.codes[tab.codeIdx];
+        const code = projectState.directory.codes[tab.codeIdx]!;
         return (
           <div className="flex h-full justify-end space-x-2 bg-white">
             <div className="flex-1">
-              <CodeMirror
-                value={code!.content}
-                theme={githubLight}
-                extensions={[javascript(), myTheme, EditorView.lineWrapping]}
-                onChange={(value, viewUpdate) => {
+              <Editor
+                defaultLanguage="javascript"
+                value={code.content}
+                path={code.fsPath}
+                options={{
+                  fontSize: 14,
+                  minimap: { enabled: false },
+                  overviewRulerBorder: false,
+                }}
+                onChange={(value, event) => {
                   const t = tab as { type: "JsEditor"; codeIdx: number };
                   projectDispatch({
                     type: "UpdateJsFile",
                     codeIdx: t.codeIdx,
-                    content: value,
+                    content: value!,
                   });
                 }}
-              ></CodeMirror>
+              />
             </div>
             <div className="border-t-1 border-l-1 ring-inset-0 mr-0 h-full w-1/5 rounded bg-gray-50 shadow-sm ring-1 ring-gray-300">
               <HttpEndpoints></HttpEndpoints>
